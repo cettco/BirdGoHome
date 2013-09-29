7b82a50ab3ac453a6c74da02049f72e1c6d6c5da
cettco <zhangshiqi68@gamil.com>
2013-09-29 11:06:21 +0800

    background

--- Classes/GameMenu.cpp
+++ Classes/GameMenu.cpp
@@ -22,7 +22,7 @@ CCScene* GameMenu::scene()
 	CCNodeLoaderLibrary *lib = CCNodeLoaderLibrary::newDefaultCCNodeLoaderLibrary();
 	lib->registerCCNodeLoader("Main", MainLayerLoader::loader());
 	CCBReader *reader = new CCBReader(lib);
-	CCNode *node = reader->readNodeGraphFromFile("Main.ccbi", scene);
+	CCNode *node = reader->readNodeGraphFromFile("Main1.ccbi", scene);
 	reader->release(); //注意手动释放内存
 	//CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
 	//pLabel->setString
